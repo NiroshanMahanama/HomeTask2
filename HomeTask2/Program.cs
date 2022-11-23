df247e9a5c5181fbc6d1eb623d46ad967af3de97
NiroshanMahanama <nmahanama@gmail.com>
2022-11-23 13:14:00 +0100

    code commit1

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -10,6 +10,28 @@ namespace HomeTask2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please insert the nominal value");
+            var nominal = Console.ReadLine();
+            var fnominal = float.Parse(nominal);
+            Console.WriteLine($"The Nominal Value is:  {fnominal}");
+
+            Console.WriteLine("Please insert the trade price");
+            var tradePrice = Console.ReadLine();
+            var ftradePrice = float.Parse(tradePrice);
+            Console.WriteLine(ftradePrice);
+            Console.WriteLine($"The Trade Price is:  {ftradePrice}");
+
+
+            /*
+            var nominal = 33.33f;
+            var tadePrice = 33.33f;
+
+            Console.WriteLine("Please input 2nd numner");
+            string secondNumber = Console.ReadLine();
+            int intsecondNumber = int.Parse(secondNumber);
+
+            */
+            Console.ReadLine();
         }
     }
 }
